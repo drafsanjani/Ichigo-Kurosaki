6e17efde8f2acb229d8d7a41c81ce65fa68cd5cf
drafsanjani <drafsanjani@gmail.com>
2022-12-27 17:32:46 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,9 +9,9 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Ubah Disini
-global.ownerNumber = ['6281540022632','6287877173955','0']
+global.ownerNumber = ['628817647193','0','0']
 global.ownerName = 'Whatsapp Developer'
-global.packname = 'Ichigo Kurosaki'
+global.packname = 'BANTENG'
 global.author = 'Whatsapp Bot 2022'
 global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi'
